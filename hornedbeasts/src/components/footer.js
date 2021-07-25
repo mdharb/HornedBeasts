009b38de08ebf092f9c99afed9bb61d5facc0ac3
mdharb <md20.harb21@gmail.com>
2021-07-25 20:28:59 +0300

    pushing lab instructions

---
+++ hornedbeasts/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <footer>
+                   &copy; Mohammad Harb
+                </footer>
+            </div>
+        )
+    }
+};
+
+export default Footer;
