7bb802b6c7ac1d0d61b3e4c5f136943615cd949a
mdharb <md20.harb21@gmail.com>
2021-07-26 17:50:01 +0300

    pushing feature 1 & 2

--- hornedbeasts/src/components/HornedBeasts.js
+++ hornedbeasts/src/components/HornedBeasts.js
@@ -2,11 +2,34 @@ import React from 'react';
 
 class HornedBeasts extends React.Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            favoriteCounter: 0
+        };
+    }
+
+
+    favorite = () => {
+        this.setState({
+            favoriteCounter: this.state.favoriteCounter + 1
+        });
+
+        alert(this.state.favoriteCounter)
+        
+    }
+
+
     render() {
+
+
         return (
+
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.imgUrl} alt="Unicorn" />
+                <img src={this.props.imgUrl} alt={this.props.title} onClick = {this.favorite} />
                 <p>{this.props.description}</p>
             </div>
         )
