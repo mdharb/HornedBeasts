26f21a51b77afcc2fbc63d181c0b386fc152b966
mdharb <md20.harb21@gmail.com>
2021-07-27 17:11:57 +0300

    pushing eslint file and debugging

--- hornedbeasts/src/components/main.js
+++ hornedbeasts/src/components/main.js
@@ -1,28 +1,23 @@
 import React from 'react';
 
-import "../App.css";
+import '../App.css';
 
 import dataArray from '../assets/data';
 import HornedBeasts from './HornedBeasts';
 
 class Main extends React.Component {
 
-    render() {
-        return(dataArray.map((value) => 
-        <div>
-            <main>
-
-                <HornedBeasts
-                  title={value.title}
-                  imgUrl= {value.image_url}
-                  description = {value.description}  />
-
-                </main>
-        </div>)
-           
-        )
-    }
+  render() {
+    return(dataArray.map((value) => 
+      <div>
+        <main>
+          <HornedBeasts
+            title={value.title}
+            imgUrl= {value.image_url}
+            description = {value.description}  />
+        </main>
+      </div>)          
+    );
+  }
 }
-
-
 export default Main;
