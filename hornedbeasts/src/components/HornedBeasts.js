257c7c31987f655e93d514cfc33a1e10f6745eb5
mdharb <md20.harb21@gmail.com>
2021-07-27 17:27:44 +0300

    code without any eslint error

--- hornedbeasts/src/components/HornedBeasts.js
+++ hornedbeasts/src/components/HornedBeasts.js
@@ -4,52 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import CardColumns from 'react-bootstrap/CardColumns';
 import Card from 'react-bootstrap/Card';
 
-
 class HornedBeasts extends React.Component {
-
   constructor(props) {
-
     super(props);
-
     this.state = {
       favoriteCounter: 0
     };
   }
 
-
     favorite = () => {
       this.setState({
         favoriteCounter: this.state.favoriteCounter + 1
       });
-      alert(this.state.favoriteCounter);        
+      alert(this.state.favoriteCounter);
     }
 
-
     render() {
-
-
       return (
-
         <div>
-          {/* <h2>{this.props.title}</h2>
-                <img src={this.props.imgUrl} alt={this.props.title} onClick = {this.favorite} />
-                <p>{this.props.description}</p> */}
-
           <CardColumns>
             <Card style={{width: '30rem', class: 'col d-flex justify-content-center'}}>
-              <Card.Img variant="top" src={this.props.imgUrl} onClick = {this.favorite} />
+              <Card.Img variant="top" src={this.props.imgUrl} onClick = {this.favorite}/>
               <Card.Body>
                 <Card.Title>{this.props.title}</Card.Title>
                 <Card.Text>
-                  {this.props.description} <br /> <span>Likes &#10084;&#65039;: {this.state.favoriteCounter}</span>
+                  {this.props.description} <br/> <span>Likes &#10084;&#65039;: {this.state.favoriteCounter}</span>
                 </Card.Text>
 
               </Card.Body>
             </Card>
-          </CardColumns>               
+          </CardColumns>
         </div>
       );
     }
 }
-
 export default HornedBeasts;
